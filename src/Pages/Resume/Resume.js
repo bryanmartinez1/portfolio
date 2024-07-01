c2a6acfb14318467c3bcbdf2c6b81f6331df9cf1
Bryan Martinez <bryanmartinez2401@gmail.com>
2024-06-30 23:51:17 -0500

    dl pdf verison

--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Accordion from "../../Components/Accordion";
 import "./Resume.css";
-import resumeJSON from "./Resume.json";
+import { resumeJSON } from "./resumeJSON";
 
 export default function Resume({ nightMode }) {
   return (
@@ -37,11 +37,11 @@ export default function Resume({ nightMode }) {
         >
           Technical Projects
         </a>
+        <button className="download">Download PDF Verison</button>
       </div>
       <Accordion
         id="contact"
         title="Contact"
-        nightMode={nightMode}
         content={
           <ul>
             {resumeJSON.contact.map((item, index) => (
@@ -53,192 +53,44 @@ export default function Resume({ nightMode }) {
       <Accordion
         id="education"
         title="Education"
-        nightMode={nightMode}
         content={
           <ul>
-            <li>Currently Enrolled in City College of New York</li>
-            <li>Major: Computer Engineering</li>
-            <li>Expected Graduation Date: Spring 2024</li>
-            <li>Relevant Courses Taken</li>
-            <ul>
-              <li>
-                Algorithms, Data Structures, Operating Systems, Software
-                Engineering, Assembly Programming, Computer Organizations,
-                Machine Learning, Scientific Programming
-              </li>
-            </ul>
+            {resumeJSON.education.map((item, index) => (
+              <> {item}</>
+            ))}
           </ul>
         }
       />
       <Accordion
         id="technicalSkills"
         title="Techical Skills"
-        nightMode={nightMode}
         content={
           <ul>
-            <li>
-              Programming Language: C++, TypeScript, JavaScript, Java, Python,
-              Rust, C
-            </li>
+            {resumeJSON.technicalSkills.map((item, index) => (
+              <li index={index}>{item}</li>
+            ))}
           </ul>
         }
       />
       <Accordion
         id="experience"
-        nightMode={nightMode}
         title="Experience"
         content={
           <>
-            <Accordion
-              innerheading={true}
-              nightMode={nightMode}
-              title="Atlassian, Software Engineer, Starting July 2024"
-              content={
-                <ul>
-                  <li>TBD</li>
-                </ul>
-              }
-            />
-            <Accordion
-              innerheading={true}
-              nightMode={nightMode}
-              title="Atlassian, Software Engineer Intern, May 2023 - August 2023"
-              content={
-                <ul>
-                  <li>
-                    Adapted a legacy feature to new visualizations and further
-                    adapted for more visualization types. Provided additional
-                    features and changes to visualization types and dashboard.
-                  </li>
-                  <li>
-                    Completed development process, including attended stand ups,
-                    design reviews, wrote spikes, conversed with designers to
-                    provide better user experience
-                  </li>
-                  <li>Programming Languages: TypeScript, JavaScript, Python</li>
-                </ul>
-              }
-            />
-            <Accordion
-              nightMode={nightMode}
-              innerheading={true}
-              title="Google, STEP Intern, May 2021 - August 2021"
-              content={
-                <ul>
-                  <li>
-                    12-week internship program on the open-source Fuchsia
-                    Operating System
-                  </li>
-                  <li>
-                    Developed a debugging tool that would aid developers with
-                    Fuchsia Driver Development
-                  </li>
-                  <li>
-                    Completed entire development process, including writing
-                    design docs, implementation, and going through design
-                    reviews
-                  </li>
-                  <li>Programming Languages: C++ and Rust</li>
-                </ul>
-              }
-            />
+            {resumeJSON.experience.map((item, index) => (
+              <>{item}</>
+            ))}
           </>
         }
       />
       <Accordion
         id="technicalProjects"
-        nightMode={nightMode}
         title="Techical Projects"
         content={
           <>
-            <Accordion
-              innerheading={true}
-              title="Inclusify - Cloud-Based Resume Webapp, August 2023 - June 2024"
-              content={
-                <ul>
-                  <li>Lasso is an ecommerce app like eBay</li>
-                  <li>
-                    Users can create an account to buy and sell products, guest
-                    users and users with accounts can search, filter, and sort
-                    through the items available for sale.
-                  </li>
-                  <li>
-                    Group project developed using an adapted version of the
-                    agile methodology
-                  </li>
-                  <li>
-                    <b>Technical Tools:</b> JavaScript, React, Firebase,
-                    AmazonS3,
-                  </li>
-                </ul>
-              }
-            />
-            <Accordion
-              nightMode={nightMode}
-              innerheading={true}
-              title="ProWrestleKnow, November 2023 - July 2024"
-              content={
-                <ul>
-                  <li>
-                    ProWrestleKnow is a pro wrestling encyclopedia and social
-                    media web app.
-                  </li>
-                  <li>
-                    Users can search through a variety of pro wrestling related
-                    categories, sign up and login, and publicly and privately
-                    converse with other users.
-                  </li>
-                  <li>
-                    Techical Tools: JavaScript, React, Python, Firebase,
-                    Back4App
-                  </li>
-                </ul>
-              }
-            />
-            <Accordion
-              nightMode={nightMode}
-              innerheading={true}
-              title="Parsetagram, 2022"
-              content={
-                <ul>
-                  <li>Lasso is an ecommerce app like eBay</li>
-                  <li>
-                    Users can create an account to buy and sell products, guest
-                    users and users with accounts can search, filter, and sort
-                    through the items available for sale.
-                  </li>
-                  <li>
-                    Group project developed using an adapted version of the
-                    agile methodology
-                  </li>
-                  <li>
-                    <b>Technical Tools:</b> Java, Back4App Parse
-                  </li>
-                </ul>
-              }
-            />
-            <Accordion
-              nightMode={nightMode}
-              innerheading={true}
-              title="Lasso - Ecommerce Webapp, 2022"
-              content={
-                <ul>
-                  <li>Lasso is an ecommerce app like eBay</li>
-                  <li>
-                    Users can create an account to buy and sell products, guest
-                    users and users with accounts can search, filter, and sort
-                    through the items available for sale.
-                  </li>
-                  <li>
-                    Group project developed using an adapted version of the
-                    agile methodology
-                  </li>
-                  <li>
-                    <b>Technical Tools:</b> JavaScript, React, Back4App Parse
-                  </li>
-                </ul>
-              }
-            />
+            {resumeJSON.technicalProjects.map((item) => (
+              <>{item}</>
+            ))}
           </>
         }
       />
--- src/Pages/Resume/resumeJSON.js
+++ src/Pages/Resume/resumeJSON.js
@@ -3,31 +3,163 @@ import Accordion from "../../Components/Accordion";
 export const resumeJSON = {
   contact: ["E-Mail: bryanmartinez2401@gmail.com"],
   education: [
+    <li>Computer Science Courses</li>,
+    <ul>
+      <li>
+        Algorithms, Data Structures, Operating Systems, Software Engineering,
+        Assembly Programming, Computer Organizations, Machine Learning,
+        Scientific Programming
+      </li>
+    </ul>,
+    <li>Electrical Engineering Courses</li>,
+    <ul>
+      <li>
+        Electrical Circuits, Linear System Analysis, Electronics, Communications
+        Theory, Digital Integrated Circuits, Electromagnetics
+      </li>
+    </ul>,
+  ],
+  technicalSkills: [
+    "C++",
+    "TypeScript",
+    "JavaScript",
+    "Java",
+    "Python",
+    "Rust",
+    "C",
+  ],
+  experience: [
     <Accordion
-      title="City College of New York - Bacherlors of Computer Engineering"
+      innerheading={true}
+      title="Atlassian, Software Engineer, Starting July 2024"
       content={
         <ul>
-          <li>Computer Science Courses</li>
-          <ul>
-            <li>
-              Algorithms, Data Structures, Operating Systems, Software
-              Engineering, Assembly Programming, Computer Organizations, Machine
-              Learning, Scientific Programming
-            </li>
-          </ul>
-          <li>Electrical Engineering Courses</li>
-          <ul>
-            <li>
-              Electrical Circuits, Linear System Analysis, Electronics,
-              Communications Theory, Digital Integrated Circuits,
-              Electromagnetics
-            </li>
-          </ul>
+          <li>TBD</li>
+        </ul>
+      }
+    />,
+    <Accordion
+      innerheading={true}
+      title="Atlassian, Software Engineer Intern, May 2023 - August 2023"
+      content={
+        <ul>
+          <li>
+            Adapted a legacy feature to new visualizations and further adapted
+            for more visualization types. Provided additional features and
+            changes to visualization types and dashboard.
+          </li>
+          <li>
+            Completed development process, including attended stand ups, design
+            reviews, wrote spikes, conversed with designers to provide better
+            user experience
+          </li>
+          <li>Programming Languages: TypeScript, JavaScript, Python</li>
+        </ul>
+      }
+    />,
+    <Accordion
+      innerheading={true}
+      title="Google, STEP Intern, May 2021 - August 2021"
+      content={
+        <ul>
+          <li>
+            12-week internship program on the open-source Fuchsia Operating
+            System
+          </li>
+          <li>
+            Developed a debugging tool that would aid developers with Fuchsia
+            Driver Development
+          </li>
+          <li>
+            Completed entire development process, including writing design docs,
+            implementation, and going through design reviews
+          </li>
+          <li>Programming Languages: C++ and Rust</li>
         </ul>
       }
     />,
   ],
-  technicalSkills: ["E-Mail: bryanmartinez2401@gmail.com"],
-  experience: ["E-Mail: bryanmartinez2401@gmail.com"],
-  technicalProjects: ["E-Mail: bryanmartinez2401@gmail.com"],
+  technicalProjects: [
+    <Accordion
+      innerheading={true}
+      title="Inclusify - Cloud-Based Resume Webapp, August 2023 - June 2024"
+      content={
+        <ul>
+          <li>Lasso is an ecommerce app like eBay</li>
+          <li>
+            Users can create an account to buy and sell products, guest users
+            and users with accounts can search, filter, and sort through the
+            items available for sale.
+          </li>
+          <li>
+            Group project developed using an adapted version of the agile
+            methodology
+          </li>
+          <li>
+            <b>Technical Tools:</b> JavaScript, React, Firebase, AmazonS3,
+          </li>
+        </ul>
+      }
+    />,
+    <Accordion
+      innerheading={true}
+      title="ProWrestleKnow, November 2023 - July 2024"
+      content={
+        <ul>
+          <li>
+            ProWrestleKnow is a pro wrestling encyclopedia and social media web
+            app.
+          </li>
+          <li>
+            Users can search through a variety of pro wrestling related
+            categories, sign up and login, and publicly and privately converse
+            with other users.
+          </li>
+          <li>Techical Tools: JavaScript, React, Python, Firebase, Back4App</li>
+        </ul>
+      }
+    />,
+    <Accordion
+      innerheading={true}
+      title="Parsetagram, 2022"
+      content={
+        <ul>
+          <li>Lasso is an ecommerce app like eBay</li>
+          <li>
+            Users can create an account to buy and sell products, guest users
+            and users with accounts can search, filter, and sort through the
+            items available for sale.
+          </li>
+          <li>
+            Group project developed using an adapted version of the agile
+            methodology
+          </li>
+          <li>
+            <b>Technical Tools:</b> Java, Back4App Parse
+          </li>
+        </ul>
+      }
+    />,
+    <Accordion
+      innerheading={true}
+      title="Lasso - Ecommerce Webapp, 2022"
+      content={
+        <ul>
+          <li>Lasso is an ecommerce app like eBay</li>
+          <li>
+            Users can create an account to buy and sell products, guest users
+            and users with accounts can search, filter, and sort through the
+            items available for sale.
+          </li>
+          <li>
+            Group project developed using an adapted version of the agile
+            methodology
+          </li>
+          <li>
+            <b>Technical Tools:</b> JavaScript, React, Back4App Parse
+          </li>
+        </ul>
+      }
+    />,
+  ],
 };
