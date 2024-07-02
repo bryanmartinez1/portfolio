5c6f499a29c58d85d47bc28a3ca0a800d9a37372
Bryan Martinez <bryanmartinez2401@gmail.com>
2024-07-02 00:47:36 -0500

    bold color

--- src/Data/AboutJSON.js
+++ src/Data/AboutJSON.js
@@ -1,13 +1,13 @@
 const about = {
   name: "Bryan Martinez",
-  // info: `Hello, I'm Bryan, a Software Engineer at Atlassian. I recently graduated City College of New York with a Bachelors in Computer Engineering. I've previously interned at Atlassian and Google. You can check out my projects and resume here.`,
+
   info: (
     <p className="infoParagraph">
-      Hello, I'm <b>Bryan</b>, a <b>Software Engineer</b> at <b>Atlassian</b>. I
-      recently graduated City College of New York with a Bachelors in{" "}
-      <b>Computer Engineering</b>. I've previously interned at{" "}
-      <b className="moreBold">Atlassian</b> and <b>Google</b>. You can check out
-      my projects and resume here.
+      Hello, I'm <b>Bryan</b>, a <b className="blueBold">Software Engineer</b>{" "}
+      at <b className="blueBold">Atlassian</b>. I recently graduated City
+      College of New York with a Bachelors in <b>Computer Engineering</b>. I've
+      previously interned at <b>Atlassian</b> and <b>Google</b>. You can check
+      out my projects and resume here.
     </p>
   ),
 };
