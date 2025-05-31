5c6f878f8fdc9650ea91f89fd772d2aafed2ab82
bryanmartinez1 <bryanmartinez2401@gmail.com>
2025-05-31 02:14:04 -0400

    resume page uppdated to use iframe with resume from s3

--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -1,102 +1,21 @@
-import Accordion from "../../Components/Accordion/Accordion.js";
 import "./Resume.css";
-import { resumeJSON } from "../../Data/ResumeJSON.js";
 
 export default function Resume({ nightMode }) {
+  const downloadResume = () => {
+    const element = document.createElement("a");
+    element.href =
+      "https://bryan-portfolio-storage.s3.us-east-2.amazonaws.com/Bryan_Martinez_Resume_Software_Engineer.pdf";
+    element.download = "Bryan_Martinez_Resume_Software_Engineer.pdf";
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
+
   return (
     <div className="resumePage">
-      <div className="navigation">
-        <div
-          className={`sectionLink ${nightMode && "sectionLinkNight"}`}
-          href="#contact"
-        >
-          Contact
-        </div>
-        <div
-          className={`sectionLink ${nightMode && "sectionLinkNight"}`}
-          href="#education"
-        >
-          Education
-        </div>
-        <a
-          className={`sectionLink ${nightMode && "sectionLinkNight"}`}
-          href="#technicalSkills"
-        >
-          Techical Skills
-        </a>
-        <a
-          className={`sectionLink ${nightMode && "sectionLinkNight"}`}
-          href="#experience"
-        >
-          Experience
-        </a>
-        <a
-          className={`sectionLink ${nightMode && "sectionLinkNight"}`}
-          href="#technicalProjects"
-        >
-          Technical Projects
-        </a>
-        <button
-          className="download"
-          style={{ color: `${!nightMode ? "inherit" : "whitesmoke"}` }}
-        >
-          Download PDF Verison
-        </button>
-      </div>
-      <Accordion
-        id="contact"
-        title="Contact"
-        content={
-          <ul>
-            {resumeJSON.contact.map((item, index) => (
-              <li index={index}>{item}</li>
-            ))}
-          </ul>
-        }
-      />
-      <Accordion
-        id="education"
-        title="Education"
-        content={
-          <ul>
-            {resumeJSON.education.map((item) => (
-              <> {item}</>
-            ))}
-          </ul>
-        }
-      />
-      <Accordion
-        id="technicalSkills"
-        title="Techical Skills"
-        content={
-          <ul>
-            {resumeJSON.technicalSkills.map((item, index) => (
-              <li index={index}>{item}</li>
-            ))}
-          </ul>
-        }
-      />
-      <Accordion
-        id="experience"
-        title="Experience"
-        content={
-          <div className="indentRight">
-            {resumeJSON.experience.map((item, index) => (
-              <>{item}</>
-            ))}
-          </div>
-        }
-      />
-      <Accordion
-        id="technicalProjects"
-        title="Techical Projects"
-        content={
-          <div className="indentRight">
-            {resumeJSON.technicalProjects.map((item) => (
-              <>{item}</>
-            ))}
-          </div>
-        }
+      <iframe
+        src="https://bryan-portfolio-storage.s3.us-east-2.amazonaws.com/Bryan_Martinez_Resume_Software_Engineer.pdf"
+        height="100%"
       />
     </div>
   );
